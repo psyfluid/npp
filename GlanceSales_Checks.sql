f7cbcfa2d5d765ee4465d4d28930eea04a774b19
psyfluid <psyfluid@gmail.com>
2018-04-28 17:28:53 +0300

    checks

--- GlanceSales_Checks.sql
+++ GlanceSales_Checks.sql
@@ -1,57 +1,62 @@
-SELECT TOP 1000
-       Warehouses._Fld540RRef AS storeRef,
+
+
+SELECT TOP 100
+       Checks._Fld1438RRef AS storeRef,
        Warehouses._Fld539RRef AS OrgRef,
-       DATEADD(DAY, DATEPART(DAY, _AccumReg2210._Period) - 1, DATEADD(MONTH, DATEPART(MONTH, _AccumReg2210._Period) - 1, DATEADD(YEAR, DATEPART(YEAR, _AccumReg2210._Period) - 2000, '2000-01-01 00:00:00'))) AS Period,
+       DATEADD(DAY, DATEPART(DAY, Checks._Date_Time) - 1, DATEADD(MONTH, DATEPART(MONTH, Checks._Date_Time) - 1, DATEADD(YEAR, DATEPART(YEAR, Checks._Date_Time) - 2000, '2000-01-01 00:00:00'))) AS Period,
        CASE
-           WHEN _AccumReg2210._Fld2935RRef = 0xA31287B97BA949794195482AF5A0D6D5 --AND _AccumReg2210._RecorderTRef = 0x00000046
-           THEN DATEADD(DAY, DATEPART(DAY, _AccumReg2210._Period) - 1, DATEADD(MONTH, DATEPART(MONTH, _AccumReg2210._Period) - 1, DATEADD(YEAR, DATEPART(YEAR, _AccumReg2210._Period) - 2000, '2000-01-01 23:59:59')))
-           WHEN _Document70._IDRRef IS NULL
-           THEN DATEADD(DAY, DATEPART(DAY, _AccumReg2210._Period) - 1, DATEADD(MONTH, DATEPART(MONTH, _AccumReg2210._Period) - 1, DATEADD(YEAR, DATEPART(YEAR, _AccumReg2210._Period) - 2000, '2000-01-01 23:59:59')))
-           ELSE DATEADD(DAY, DATEPART(DAY, _Document70._Date_Time) - 1, DATEADD(MONTH, DATEPART(MONTH, _Document70._Date_Time) - 1, DATEADD(YEAR, DATEPART(YEAR, _Document70._Date_Time) - 2000, '2000-01-01 23:59:59')))
+           WHEN Checks._Fld1429RRef = 0xA4FEEDD07DA60D484EE48B6C4BE07A6A
+           THEN DATEADD(DAY, DATEPART(DAY, Checks._Date_Time) - 1, DATEADD(MONTH, DATEPART(MONTH, Checks._Date_Time) - 1, DATEADD(YEAR, DATEPART(YEAR, Checks._Date_Time) - 2000, '2000-01-01 23:59:59')))
+           ELSE DATEADD(DAY, DATEPART(DAY, ChecksSale._Date_Time) - 1, DATEADD(MONTH, DATEPART(MONTH, ChecksSale._Date_Time) - 1, DATEADD(YEAR, DATEPART(YEAR, ChecksSale._Date_Time) - 2000, '2000-01-01 23:59:59')))
        END AS SalesDate,
        CASE
-           WHEN _AccumReg2210._Fld2935RRef = 0xA31287B97BA949794195482AF5A0D6D5
+           WHEN Checks._Fld1429RRef = 0xA4FEEDD07DA60D484EE48B6C4BE07A6A
            THEN CAST('Продажа' AS NVARCHAR(9))
            ELSE CAST('Возврат' AS NVARCHAR(9))
        END AS OperationType,
-       _AccumReg2210._Fld2212RRef AS goodsRef,
-       _AccumReg2210._Fld2213RRef AS characteristicRef,
-       CAST(SUM(_AccumReg2210._Fld2216) AS NUMERIC(18, 0)) AS Quantity,
+       ChecksGoods._Fld1450RRef AS goodsRef,
+       ChecksGoods._Fld1451RRef AS characteristicRef,
+       CAST(SUM(ChecksGoods._Fld1454) AS NUMERIC(18, 0)) AS Quantity,
        CASE
-           WHEN SUM(_AccumReg2210._Fld2218) = 0
+           WHEN SUM(_Document86_VT1448._Fld1455) = 0
            THEN 0
-           ELSE CAST(CAST(100 AS NUMERIC(3, 0)) - CAST(CAST(100 AS NUMERIC(3, 0)) AS NUMERIC(11, 8)) * CAST(SUM(_AccumReg2210._Fld2217) AS NUMERIC(21, 2)) / CAST(SUM(_AccumReg2210._Fld2218) AS NUMERIC(21, 2)) AS NUMERIC(3, 0))
+           ELSE CAST(CAST(100 AS NUMERIC(3, 0)) - CAST(CAST(100 AS NUMERIC(3, 0)) AS NUMERIC(11, 8)) * CAST(SUM(_Document86_VT1448._Fld1456) AS NUMERIC(21, 2)) / CAST(SUM(_Document86_VT1448._Fld1454 * _Document86_VT1448._Fld1455) AS NUMERIC(21, 2)) AS NUMERIC(3, 0))
        END AS DiscountPercent,
-       CAST(SUM(_AccumReg2210._Fld2217) AS NUMERIC(21, 2)) AS SumTotal
+       CAST(SUM(_Document86_VT1448._Fld1456) AS NUMERIC(21, 2)) AS SumTotal
 --INTO
 --     #Sales
 FROM
-     _AccumReg2210 _AccumReg2210 WITH (NOLOCK)
+     _Document86_VT1448 ChecksGoods WITH (NOLOCK)
+     INNER JOIN _Document86 Checks WITH (NOLOCK)
+          ON ChecksGoods._Document86_IDRRef = Checks._IDRRef
+             AND Checks._Date_Time >= '2017-01-01 00:00:00'
+             AND Checks._Marked = 0x00
+             AND (Checks._Fld1442RRef = 0x89F57AF1607D02E348AA4420D45671C8
+                  OR Checks._Fld1442RRef = 0xA5BC0794150D43594A2DACE5EDBD2DDA)
+     LEFT OUTER JOIN _Document86 ChecksSale WITH (NOLOCK)
+          ON Checks._Fld1439RRef = ChecksSale._IDRRef
+     LEFT OUTER JOIN _Reference29 Stores WITH (NOLOCK)
+          ON Checks._Fld1438RRef = Stores._IDRRef
      LEFT OUTER JOIN _Reference39 Warehouses WITH (NOLOCK)
-          ON _AccumReg2210._Fld2211RRef = Warehouses._IDRRef
-     LEFT OUTER JOIN _Document70 WITH (NOLOCK)
-          ON _AccumReg2210._Fld2214_TYPE = 0x08
-             AND _AccumReg2210._Fld2214_RTRef = 0x00000046
-             AND _AccumReg2210._Fld2214_RRRef = _Document70._IDRRef
-WHERE  _AccumReg2210._Period >= '2017-01-01 00:00:00'
+          ON Stores._Fld310RRef = Warehouses._IDRRef
 GROUP BY
-         Warehouses._Fld540RRef,
+         Checks._Fld1438RRef,
          Warehouses._Fld539RRef,
-         DATEADD(DAY, DATEPART(DAY, _AccumReg2210._Period) - 1, DATEADD(MONTH, DATEPART(MONTH, _AccumReg2210._Period) - 1, DATEADD(YEAR, DATEPART(YEAR, _AccumReg2210._Period) - 2000, '2000-01-01 00:00:00'))),
+         DATEADD(DAY, DATEPART(DAY, Checks._Date_Time) - 1, DATEADD(MONTH, DATEPART(MONTH, Checks._Date_Time) - 1, DATEADD(YEAR, DATEPART(YEAR, Checks._Date_Time) - 2000, '2000-01-01 00:00:00'))),
          CASE
-             WHEN _AccumReg2210._Fld2935RRef = 0xA31287B97BA949794195482AF5A0D6D5 --AND _AccumReg2210._RecorderTRef = 0x00000046
-             THEN DATEADD(DAY, DATEPART(DAY, _AccumReg2210._Period) - 1, DATEADD(MONTH, DATEPART(MONTH, _AccumReg2210._Period) - 1, DATEADD(YEAR, DATEPART(YEAR, _AccumReg2210._Period) - 2000, '2000-01-01 23:59:59')))
-             WHEN _Document70._IDRRef IS NULL
-             THEN DATEADD(DAY, DATEPART(DAY, _AccumReg2210._Period) - 1, DATEADD(MONTH, DATEPART(MONTH, _AccumReg2210._Period) - 1, DATEADD(YEAR, DATEPART(YEAR, _AccumReg2210._Period) - 2000, '2000-01-01 23:59:59')))
-             ELSE DATEADD(DAY, DATEPART(DAY, _Document70._Date_Time) - 1, DATEADD(MONTH, DATEPART(MONTH, _Document70._Date_Time) - 1, DATEADD(YEAR, DATEPART(YEAR, _Document70._Date_Time) - 2000, '2000-01-01 23:59:59')))
+             WHEN Checks._Fld1429RRef = 0xA4FEEDD07DA60D484EE48B6C4BE07A6A
+             THEN DATEADD(DAY, DATEPART(DAY, Checks._Date_Time) - 1, DATEADD(MONTH, DATEPART(MONTH, Checks._Date_Time) - 1, DATEADD(YEAR, DATEPART(YEAR, Checks._Date_Time) - 2000, '2000-01-01 23:59:59')))
+             ELSE DATEADD(DAY, DATEPART(DAY, ChecksSale._Date_Time) - 1, DATEADD(MONTH, DATEPART(MONTH, ChecksSale._Date_Time) - 1, DATEADD(YEAR, DATEPART(YEAR, ChecksSale._Date_Time) - 2000, '2000-01-01 23:59:59')))
          END,
          CASE
-             WHEN _AccumReg2210._Fld2935RRef = 0xA31287B97BA949794195482AF5A0D6D5
+             WHEN Checks._Fld1429RRef = 0xA4FEEDD07DA60D484EE48B6C4BE07A6A
              THEN CAST('Продажа' AS NVARCHAR(9))
              ELSE CAST('Возврат' AS NVARCHAR(9))
          END,
-         _AccumReg2210._Fld2212RRef,
-         _AccumReg2210._Fld2213RRef
-
+         ChecksGoods._Fld1450RRef,
+         ChecksGoods._Fld1451RRef
 ORDER BY
-    storeRef, Period, goodsRef, characteristicRef
+         storeRef,
+         Period,
+         goodsRef,
+         characteristicRef;
